1efdd2dd7678d28b4548cffc0f0a684c37a655a6
MikeMike39 <Mfonseca294@gmail.com>
2021-07-01 08:52:54 -0400

    Done

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+ def parrot(phrase = "Squawk!")
+     puts   phrase
+     phrase
+end
+
+# def parrot(name, phrase = "Squawk")
+#     puts("Hi I'm #{name}. I like to #{phrase}.")
+#     return parrot
+# end
+
+# parrot("Jerry", "play ball")
